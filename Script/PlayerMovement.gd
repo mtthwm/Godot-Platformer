46233a22fec76d6fe177716261a32687a1aba5bb
Matthew Morales <mtthwmrls9@gmail.com>
2024-07-07 22:36:05 -0600

    Fix grappling

--- Script/PlayerMovement.gd
+++ Script/PlayerMovement.gd
@@ -9,18 +9,20 @@ extends RigidBody2D
 @export var groundCheck: GroundChecker;
 @export var leftWallChecker: GroundChecker;
 @export var rightWallChecker: GroundChecker;
+@export var ceilingChecker: GroundChecker;
 @export var extraJumps = 1;
 
 @export_group("Grappling")
-@export var grappleLength = 200;
+@export var grappleLengthTiles = 12;
 @export var crosshair: Node2D;
+@export var grappleSpeed = 16;
+@export var minGrappleTileLength = 2
 
+var _locked_movement: int = 0;
 
 var _grapplePoint: Vector2;
 var _grappling: bool = false;
 var _grappleDir: Vector2;
-var _canGrapple: bool = false;
-var _aiming: bool = false;
 
 var _maxSpeed;
 var _gravity_magnitude;
@@ -51,7 +53,7 @@ func _should_jump ():
 	var heldLongEnough = Input.is_action_pressed("move_jump") && (_timeSincePress > inputMaxFullHoldTime);
 	return justReleased or (heldLongEnough and not _hasJumpedThisButtonPress);
 
-func _doJump ():
+func _do_jump ():
 	_remainingJumps -= 1;
 	var holdTimeModifier = min(_timeSincePress / inputMaxFullHoldTime, 1);
 	var jumpVelocity = sqrt(2 * _weight * _jumpHeight * holdTimeModifier);
@@ -60,7 +62,7 @@ func _doJump ():
 	linear_velocity.y -= jumpVelocity;
 	_hasJumpedThisButtonPress = true;
 
-func _handle_jumping (delta):
+func _handle_jump (delta):
 	_timeSincePress += delta;
 	
 	if groundCheck.isGrounded:
@@ -70,7 +72,7 @@ func _handle_jumping (delta):
 		_timeSincePress = 0;
 
 	if _can_jump() and _should_jump():
-		_doJump();
+		_do_jump();
 
 	if Input.is_action_just_released("move_jump"):
 		_hasJumpedThisButtonPress = false;
@@ -88,60 +90,80 @@ func _handle_grappling (_delta):
 	if Input.is_action_pressed("move_down"):
 		_grappleDir.y = 1;
 
-	_grappleDir = _grappleDir.normalized();
-
-	_grapplePoint = global_position + _grappleDir * grappleLength;
+	if !_grappling:
+		_grappleDir = _grappleDir.normalized();
+		_grapplePoint = global_position + _grappleDir * grappleLengthTiles * tilesize;
 
 	if Input.is_action_pressed("move_grapple"):
-		var space_state = get_world_2d().direct_space_state
-		var query = PhysicsRayQueryParameters2D.create(global_position, _grapplePoint);
-		var result = space_state.intersect_ray(query);
+		_lock_movement(3);
+		
+		if !_grappling:
+			var space_state = get_world_2d().direct_space_state
+			var query = PhysicsRayQueryParameters2D.create(global_position, _grapplePoint);
+			var result = space_state.intersect_ray(query);
 
-		if result:
-			_canGrapple = true;
-			crosshair.modulate = Color(1, 1, 1);
-			_grapplePoint = result.position;
-		else:
-			crosshair.modulate = Color(1, 0, 0);
-			_canGrapple = false;
+			if result && position.distance_to(result.position) > minGrappleTileLength * tilesize:
+				crosshair.modulate = Color(1, 1, 1);
+				_grapplePoint = result.position;
+				_grappling = true;
+				_lock_movement(5);
 
-		crosshair.show();
-		crosshair.set_global_position(_grapplePoint);
+			else:
+				crosshair.modulate = Color(1, 0, 0);
 
-		_aiming = true;
-			
+			crosshair.show();
+			crosshair.set_global_position(_grapplePoint);
 	else:
 		crosshair.hide();
-
-		_aiming = false;
-
-	if Input.is_action_just_pressed("move_grapple") && _canGrapple:
-		_grappling = true;
-		_canGrapple = false;
+		_unlock_movement(3);
 
 	if _grappling:
-		if groundCheck.isGrounded:
+		var touchingAnything: bool = (groundCheck.isGrounded || leftWallChecker.isGrounded || rightWallChecker.isGrounded || ceilingChecker.isGrounded);
+		if touchingAnything || !Input.is_action_pressed("move_grapple"):
 			_grappling = false;
-		
+			_unlock_movement(5);
+			_grappling = false;
+			
+func _grappling_velocity ():
+	var toGrapplePoint = (_grapplePoint - global_position).normalized();
+	var rotateAngle = PI/2;
+	if _grappleDir.x < 0:
+		rotateAngle = -rotateAngle;
+	print_debug(_grapplePoint);
+	var tangent = toGrapplePoint.rotated(rotateAngle);
+	return grappleSpeed * tilesize * tangent;
 
 func _handle_movement (_delta):
 	var movement_force_magnitude = (_maxSpeed / timeToMaxSpeed) * mass + _normalForceMagnitude;
 	var frictional_force_magnitude = _normalForceMagnitude * physics_material_override.friction;
 	var horizontalForce = movement_force_magnitude+ frictional_force_magnitude;
 
-	if !_aiming && !_grappling && Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("move_right") && !_locked_movement:
 		if !rightWallChecker.isGrounded:
 			if linear_velocity.x < _maxSpeed:
 				apply_force(Vector2(horizontalForce, 0));
 
-	if !_aiming && !_grappling && Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("move_left") && !_locked_movement:
 		if !leftWallChecker.isGrounded:
-			print_debug("LEFT WALL NOT GROUNDED");
 			if linear_velocity.x > -_maxSpeed:
 				apply_force(Vector2(-horizontalForce, 0));
+
+func _lock_movement (index):
+	_locked_movement |= (1 << index);
+
+func _unlock_movement (index):
+	_locked_movement &= ~(1 << index);
+
+func _handle_swing (_delta):
+	if _grappling:
+		gravity_scale = 0;
+		linear_velocity = _grappling_velocity();
+	else:
+		gravity_scale = 1;
 
 
 func _physics_process(delta):
 	_handle_movement(delta);
-	_handle_jumping(delta);
+	_handle_jump(delta);
 	_handle_grappling(delta);
+	_handle_swing(delta);
